7bc6eccb7ff9af284ee108dd1aaec6fcb57e531a
alvrba <alvaro.b72@gmail.com>
2022-03-14 17:26:54 +0100

    change paragraph font size

--- src/components/Root.js
+++ src/components/Root.js
@@ -5,6 +5,7 @@ import ReduxToastr from 'react-redux-toastr';
 import { grey } from '@material-ui/core/colors';
 import 'react-redux-toastr/lib/css/react-redux-toastr.min.css';
 import { createTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { GlobalStyles } from '@mui/material';
 import App from './App';
 import configureStore from '../store/configure';
 import i18nConfig from '../config/i18n';
@@ -35,12 +36,15 @@ const theme = createTheme({
   },
 });
 
+const globalStyles = <GlobalStyles styles={{ p: { fontSize: '1rem' } }} />;
+
 const { store } = configureStore();
 
 const Root = () => (
   <QueryClientProvider client={queryClient}>
     <I18nextProvider i18n={i18nConfig}>
       <Provider store={store}>
+        {globalStyles}
         <MuiThemeProvider theme={theme}>
           {SHOW_NOTIFICATIONS && <ReduxToastr />}
           <App />
