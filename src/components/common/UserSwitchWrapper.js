bdab98c18c9791390a19aa732ab95729b00f3c0e
pyphilia <pyphilia@gmail.com>
2022-06-03 13:39:11 +0200

    feat: add user switch

---
+++ src/components/common/UserSwitchWrapper.js
@@ -0,0 +1,59 @@
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router';
+import { useTranslation } from 'react-i18next';
+import PropTypes from 'prop-types';
+import { UserSwitchWrapper as GraaspUserSwitch } from '@graasp/ui';
+import { MUTATION_KEYS } from '@graasp/query-client';
+
+import { useMutation, hooks } from '../../config/queryClient';
+import {
+  DOMAIN,
+  SIGN_IN_PATH,
+  MEMBER_PROFILE_PATH,
+} from '../../config/constants';
+import { CurrentMemberContext } from '../context/CurrentMemberContext';
+
+const UserSwitchWrapper = ({ ButtonContent }) => {
+  const {
+    data: member,
+    isLoading,
+    isSuccess: isSuccessUser,
+  } = useContext(CurrentMemberContext);
+  const navigate = useNavigate();
+  const { t } = useTranslation();
+  const { mutateAsync: signOut } = useMutation(MUTATION_KEYS.SIGN_OUT);
+  const { mutate: switchUser } = useMutation(MUTATION_KEYS.SWITCH_MEMBER);
+
+  return (
+    <>
+      <GraaspUserSwitch
+        ButtonContent={ButtonContent}
+        navigate={navigate}
+        signOut={signOut}
+        currentMember={member}
+        isCurrentMemberLoading={isLoading}
+        isCurrentMemberSuccess={isSuccessUser}
+        useAvatar={hooks.useAvatar}
+        switchMember={switchUser}
+        seeProfileText={t('See Profile')}
+        signedOutTooltipText={t('You are not signed in.')}
+        signOutText={t('Sign Out')}
+        switchMemberText={t('Sign in with another account')}
+        profilePath={MEMBER_PROFILE_PATH}
+        domain={DOMAIN}
+        redirectPath={SIGN_IN_PATH}
+        useMembers={hooks.useMembers}
+      />
+    </>
+  );
+};
+
+UserSwitchWrapper.propTypes = {
+  ButtonContent: PropTypes.node,
+};
+
+UserSwitchWrapper.defaultProps = {
+  ButtonContent: null,
+};
+
+export default UserSwitchWrapper;
