1fad28b546ea9cb3e79d261490fbb3ac08b0a816
pyphilia <pyphilia@gmail.com>
2022-06-20 15:12:21 +0200

    ci: upgrade yarn

--- src/components/context/CurrentMemberContext.js
+++ src/components/context/CurrentMemberContext.js
@@ -17,7 +17,6 @@ const CurrentMemberContextProvider = ({ children }) => {
     }
   }, [lang]);
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   const value = useMemo(() => query, [query.data]);
 
   return (
