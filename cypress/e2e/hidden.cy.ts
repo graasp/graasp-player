4fbf3106e24972b633fb5d6fab2960baa3611d71
spaenleh <spaenleh@gmail.com>
2023-09-22 09:36:38 +0200

    fix: hidden tests

--- cypress/e2e/hidden.cy.ts
+++ cypress/e2e/hidden.cy.ts
@@ -10,7 +10,7 @@ import {
 import { MEMBERS } from '../fixtures/members';
 
 describe('Hidden Items', () => {
-  it('Display Hidden items with hidden wrapper', () => {
+  it("Don't display Hidden items when viewing as admin", () => {
     cy.setUpApi({
       items: FOLDER_WITH_HIDDEN_ITEMS.items,
     });
@@ -18,18 +18,39 @@ describe('Hidden Items', () => {
     const parent = FOLDER_WITH_HIDDEN_ITEMS.items[0];
     cy.visit(buildMainPath({ rootId: parent.id }));
 
-    cy.wait(['@getCurrentMember', '@getItem', '@getItemTags']);
-    cy.get(`#${buildDocumentId(FOLDER_WITH_HIDDEN_ITEMS.items[1].id)}`).should(
-      'exist',
+    cy.get(
+      `#${buildHiddenWrapperId(FOLDER_WITH_HIDDEN_ITEMS.items[1].id, false)}`,
+    ).should('exist');
+
+    // hidden document should not be displayed when in public
+    cy.get(`#${buildDocumentId(FOLDER_WITH_HIDDEN_ITEMS.items[2].id)}`).should(
+      'not.exist',
     );
-    // hidden item should have wrapper
     cy.get(
       `#${buildHiddenWrapperId(FOLDER_WITH_HIDDEN_ITEMS.items[2].id, true)}`,
-    );
-    // hidden elements should not be shown in the navigation
+    ).should('not.exist');
+  });
+
+  it("Don't display Hidden items when viewing as writer", () => {
+    cy.setUpApi({
+      currentMember: MEMBERS.CEDRIC,
+      items: FOLDER_WITH_HIDDEN_ITEMS.items,
+    });
+
+    const parent = FOLDER_WITH_HIDDEN_ITEMS.items[0];
+    cy.visit(buildMainPath({ rootId: parent.id }));
+
     cy.get(
-      `#${buildHiddenWrapperId(FOLDER_WITH_HIDDEN_ITEMS.items[3].id, true)}`,
+      `#${buildHiddenWrapperId(FOLDER_WITH_HIDDEN_ITEMS.items[1].id, false)}`,
+    ).should('exist');
+
+    // hidden document should not be displayed when in public
+    cy.get(`#${buildDocumentId(FOLDER_WITH_HIDDEN_ITEMS.items[2].id)}`).should(
+      'not.exist',
     );
+    cy.get(
+      `#${buildHiddenWrapperId(FOLDER_WITH_HIDDEN_ITEMS.items[2].id, true)}`,
+    ).should('not.exist');
   });
 
   it("Don't display Hidden items when viewing as reader", () => {
