eaa5995b7f683f70e18660a26b5f5ba8671c2817
spaenleh <spaenleh@gmail.com>
2023-10-03 07:30:18 +0200

    fix: remove params

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,44 @@
-import { useLocation } from 'react-router';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
+import {
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+  useSearchParams,
+} from 'react-router-dom';
 
-import { saveUrlForRedirection } from '@graasp/sdk';
+import { hasAcceptedCookies, saveUrlForRedirection } from '@graasp/sdk';
 import { CustomInitialLoader, withAuthorization } from '@graasp/ui';
 
 import { SIGN_IN_PATH } from '@/config/constants';
-import { DOMAIN } from '@/config/env';
+import { DOMAIN, GA_MEASUREMENT_ID } from '@/config/env';
 import { HOME_PATH, buildMainPath } from '@/config/paths';
 import { useCurrentMemberContext } from '@/contexts/CurrentMemberContext';
 import HomePage from '@/modules/pages/HomePage';
 import ItemPage from '@/modules/pages/ItemPage';
 
 export const App = (): JSX.Element => {
-  const { pathname } = useLocation();
+  const location = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
   const { data: currentMember, isLoading } = useCurrentMemberContext();
+
+  useEffect(() => {
+    // REACTGA
+    // Send pageview with a custom path
+    if (GA_MEASUREMENT_ID && hasAcceptedCookies()) {
+      ReactGA.initialize(GA_MEASUREMENT_ID);
+      ReactGA.send('pageview');
+    }
+
+    // remove cross domain tracking query params
+    // eslint-disable-next-line no-console
+    console.log('removing google cross site tracking params after load');
+    // eslint-disable-next-line no-console
+    console.log(searchParams);
+    searchParams.delete('_gl');
+    setSearchParams(searchParams);
+  }, [location]);
 
   if (isLoading) {
     return <CustomInitialLoader />;
@@ -24,7 +49,7 @@ export const App = (): JSX.Element => {
     redirectionLink: SIGN_IN_PATH,
     onRedirect: () => {
       // save current url for later redirection after sign in
-      saveUrlForRedirection(pathname, DOMAIN);
+      saveUrlForRedirection(location.pathname, DOMAIN);
     },
   };
   const HomePageWithAuthorization = withAuthorization(HomePage, props);
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
